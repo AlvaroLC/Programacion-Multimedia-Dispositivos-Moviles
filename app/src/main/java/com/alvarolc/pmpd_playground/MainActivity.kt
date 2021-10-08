447e14cb3760995358d407f01993dde4e1189833
AlvaroLC <alvarolopezcab21@gmail.com>
2021-10-08 19:26:26 +0200

    Version (08/10/2021)

--- app/src/main/java/com/alvarolc/pmpd_playground/MainActivity.kt
+++ app/src/main/java/com/alvarolc/pmpd_playground/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alvarolc.pmpd_playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
 
@@ -9,10 +10,37 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("@dev","onCreate")
         setContentView(R.layout.activity_main)
 
-        val imageLion : AppCompatImageView = findViewById(R.id.image_lion)
+        //val imageLion : AppCompatImageView = findViewById(R.id.ic_image_lion)
         //imageLion.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_image_lion))
-        imageLion.setImageResource(R.drawable.ic_image_lion)
+        //imageLion.setImageResource(R.drawable.ic_image_lion)
+
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.d("@dev","onStart")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d("@dev","onResume")
+    }
+
+    override fun onPause(){
+        super.onPause()
+        Log.d("@dev","onPause")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.d("@dev","onStop")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d("@dev","onDestroy")
     }
 }
---
+++ app/src/main/java/com/alvarolc/pmpd_playground/ut3/UiActivity.kt
@@ -0,0 +1,12 @@
+package com.alvarolc.pmpd_playground.ut3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alvarolc.pmpd_playground.R
+
+class UiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui)
+    }
+}
