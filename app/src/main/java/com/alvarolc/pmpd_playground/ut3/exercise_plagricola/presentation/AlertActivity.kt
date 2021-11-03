36981654f17064256a0a97720ea1271438758372
AlvaroLC <alvarolopezcab21@gmail.com>
2021-11-03 18:40:50 +0100

    Creada carpetas plagricolas

--- app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertActivity.kt
+++ app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertActivity.kt
@@ -2,6 +2,7 @@ package com.alvarolc.pmpd_playground.ut3.exercise_plagricola.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alvarolc.pmpd_playground.R
 
 class AlertActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
