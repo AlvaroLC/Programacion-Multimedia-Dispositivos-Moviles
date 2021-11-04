55ed0374329aa9c92144683045c5b889740526d8
AlvaroLC <alvarolopezcab21@gmail.com>
2021-11-04 16:40:23 +0100

    (04/11/2021)

--- app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertActivity.kt
+++ app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertActivity.kt
@@ -3,10 +3,18 @@ package com.alvarolc.pmpd_playground.ut3.exercise_plagricola.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alvarolc.pmpd_playground.R
+import com.alvarolc.pmpd_playground.ut3.exercise_plagricola.domain.AlertModel
 
 class AlertActivity : AppCompatActivity() {
+
+    private val alertViewModel = AlertViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alert)
+        render()
+    }
+
+    private fun render(){
+        val alertModel = alertViewModel.getAllAlertModel()
     }
 }
--- app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertViewModel.kt
+++ app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertViewModel.kt
@@ -3,5 +3,5 @@ package com.alvarolc.pmpd_playground.ut3.exercise_plagricola.presentation
 import com.alvarolc.pmpd_playground.ut3.exercise_plagricola.domain.AlertModel
 
 class AlertViewModel {
-    //fun getAllAlertModel() = AlertModel()
+    fun getAllAlertModel() = AlertModel()
 }
