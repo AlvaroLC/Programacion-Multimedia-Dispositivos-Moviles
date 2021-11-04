03aa462d19a52b13a0d2f58c98713ddecfb7f0f4
AlvaroLC <alvarolopezcab21@gmail.com>
2021-11-04 16:21:08 +0100

    (04/11/2021)

--- app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertViewModel.kt
+++ app/src/main/java/com/alvarolc/pmpd_playground/ut3/exercise_plagricola/presentation/AlertViewModel.kt
@@ -1,4 +1,7 @@
 package com.alvarolc.pmpd_playground.ut3.exercise_plagricola.presentation
 
+import com.alvarolc.pmpd_playground.ut3.exercise_plagricola.domain.AlertModel
+
 class AlertViewModel {
+    //fun getAllAlertModel() = AlertModel()
 }
